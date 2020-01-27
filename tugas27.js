6655b43d5cd26df53d20deb9ebf70c05e20f7975
sinar96 <sinar96>
2020-01-27 20:18:45 +0800

    convert to string

---
+++ tugas27.js
@@ -0,0 +1,13 @@
+ var a = "No";
+ var b = " rumah saya ";
+ var c = 30;
+ var d = "A";
+
+hasil1 = a + b + String(c) + d;
+console.log(hasil1);
+
+var e = "Jalan pangeran Tirtayasa No";
+var f = 70;
+
+hasil2 = e + f;
+console.log(hasil2);
